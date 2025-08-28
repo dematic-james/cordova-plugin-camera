f7a722f3e05d5d5578ba7e70ac2909a49ad3c317
James Burrows <james.burrows@dematic.com>
2025-08-28 14:29:08 -0600

    Remove wrapper

--- www/Camera.js
+++ www/Camera.js
@@ -1,5 +1,4 @@
-cordova.define('cordova-plugin-camera.camera', function (require, exports, module) {
-                /*
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -187,5 +186,3 @@ cameraExport.cleanup = function (successCallback, errorCallback) {
 };
 
 module.exports = cameraExport;
-
-            });
